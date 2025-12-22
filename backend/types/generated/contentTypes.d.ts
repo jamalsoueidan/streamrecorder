1187856e246ad4d2212e94766d9daf6e6b7e36ad
Jamal Soueidan <jamal@soueidan.com>
2025-12-22 10:14:52 +0100

    add lastCheckedAt and coolify docker files

--- backend/types/generated/contentTypes.d.ts
+++ backend/types/generated/contentTypes.d.ts
@@ -444,6 +444,7 @@ export interface ApiFollowerFollower extends Struct.CollectionTypeSchema {
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
+    lastCheckedAt: Schema.Attribute.DateTime;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
       'oneToMany',
