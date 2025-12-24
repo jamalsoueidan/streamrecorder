34a13daa41fe9c98b9a88522fdef22b080f9f75d
Jamal Soueidan <jamal@soueidan.com>
2025-12-24 01:23:34 +0100

    add CSP for strapi

--- backend/config/middlewares.ts
+++ backend/config/middlewares.ts
@@ -1,12 +1,26 @@
-export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+export default ({ env }) => [
+  "strapi::logger",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": ["'self'", "data:", "blob:", env("AWS_ENDPOINT")],
+          "media-src": ["'self'", "data:", "blob:", env("AWS_ENDPOINT")],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  "strapi::errors",
+  "strapi::security",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
