1bf7436ad680eb5a120bb60788aa16f6d3b49d98
Jamal Soueidan <jamal@soueidan.com>
2026-01-07 23:46:50 +0100

    add missing pages to sitemaps

--- frontend/src/app/sitemaps/static.xml/route.ts
+++ frontend/src/app/sitemaps/static.xml/route.ts
@@ -7,7 +7,8 @@ export async function GET() {
 
   const staticPages = [
     { url: "", priority: 1.0, changefreq: "daily" }, // Homepage
-    //{ url: "/about", priority: 0.7, changefreq: "monthly" },
+    { url: "/news", priority: 0.7, changefreq: "weekly" },
+    { url: "/product-updates", priority: 0.4, changefreq: "weekly" },
     { url: "/pricing", priority: 0.8, changefreq: "weekly" },
     { url: "/faq", priority: 0.6, changefreq: "monthly" },
     //{ url: "/contact", priority: 0.5, changefreq: "monthly" },
