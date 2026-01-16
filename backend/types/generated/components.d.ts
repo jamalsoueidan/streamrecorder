83629ec05a4d20d214dc4653fabd6e1d1a0cf3ba
Jamal Soueidan <jamal@soueidan.com>
2026-01-16 10:33:59 +0100

    remove navigation no need for that anymore

--- backend/types/generated/components.d.ts
+++ backend/types/generated/components.d.ts
@@ -1,56 +1,5 @@
 import type { Schema, Struct } from '@strapi/strapi';
 
-export interface NavigationsLinks extends Struct.ComponentSchema {
-  collectionName: 'components_navigations_links';
-  info: {
-    displayName: 'links';
-  };
-  attributes: {
-    color: Schema.Attribute.String;
-    icon: Schema.Attribute.Enumeration<
-      [
-        'IconFlower',
-        'IconUsers',
-        'IconPlayerPlayFilled',
-        'IconWorldSearch',
-        'IconQuestionMark',
-        'IconVideo',
-        'IconPlayerRecord',
-        'IconBrandSafari',
-        'IconHeart',
-        'IconStar',
-      ]
-    >;
-    label: Schema.Attribute.String;
-    url: Schema.Attribute.String;
-  };
-}
-
-export interface NavigationsSections extends Struct.ComponentSchema {
-  collectionName: 'components_navigations_sections';
-  info: {
-    displayName: 'sections';
-  };
-  attributes: {
-    icon: Schema.Attribute.Enumeration<
-      [
-        'IconFlower',
-        'IconUsers',
-        'IconPlayerPlayFilled',
-        'IconWorldSearch',
-        'IconQuestionMark',
-        'IconVideo',
-        'IconPlayerRecord',
-        'IconBrandSafari',
-        'IconHeart',
-        'IconStar',
-      ]
-    >;
-    links: Schema.Attribute.Component<'navigations.links', true>;
-    title: Schema.Attribute.String;
-  };
-}
-
 export interface VideosVideo extends Struct.ComponentSchema {
   collectionName: 'components_videos_videos';
   info: {
@@ -67,8 +16,6 @@ export interface VideosVideo extends Struct.ComponentSchema {
 declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
-      'navigations.links': NavigationsLinks;
-      'navigations.sections': NavigationsSections;
       'videos.video': VideosVideo;
     }
   }
