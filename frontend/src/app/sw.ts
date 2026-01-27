f5f0579c9585099bded93162b1d39c2149c023d8
Jamal Soueidan <jamal@soueidan.com>
2026-01-27 17:29:58 +0100

    fix follower not found

--- frontend/src/app/sw.ts
+++ frontend/src/app/sw.ts
@@ -1,7 +1,6 @@
 /// <reference no-default-lib="true" />
 /// <reference lib="esnext" />
 /// <reference lib="webworker" />
-import { defaultCache } from "@serwist/turbopack/worker";
 import type { PrecacheEntry, SerwistGlobalConfig } from "serwist";
 import { Serwist } from "serwist";
 
@@ -14,11 +13,8 @@ declare global {
 declare const self: ServiceWorkerGlobalScope;
 
 const serwist = new Serwist({
-  precacheEntries: self.__SW_MANIFEST,
   skipWaiting: true,
   clientsClaim: true,
-  navigationPreload: true,
-  runtimeCaching: defaultCache,
 });
 
 serwist.addEventListeners();
