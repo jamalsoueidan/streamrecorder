7fd39dcb0540f86e4ba83aebe26bcd8446cd3b45
Jamal Soueidan <jamal@soueidan.com>
2026-04-16 23:18:11 +0200

    update all urls to use s3

--- frontend/src/app/lib/avatar-url.ts
+++ frontend/src/app/lib/avatar-url.ts
@@ -1,13 +1,17 @@
+const MEDIA_PROXY = process.env.NEXT_PUBLIC_MEDIA_PROXY_HOST;
+
 export function generateAvatarUrl(url: string, fullUrl?: boolean): string;
 export function generateAvatarUrl(url: undefined | null, fullUrl?: boolean): undefined;
 export function generateAvatarUrl(url?: string | null, fullUrl?: boolean): string | undefined;
 export function generateAvatarUrl(url?: string | null, fullUrl: boolean = false) {
   if (!url) return undefined;
+  if (MEDIA_PROXY) {
+    const filename = url.split("/").pop();
+    return `https://${MEDIA_PROXY}/streamavatars-nbg/thumbnail_${filename}`;
+  }
   const filename = url.split("/").pop();
-  const location = url.includes("nbg1") ? "nbg1" : "";
-  const query = location ? `?location=${location}` : "";
   return (
     (fullUrl ? process.env.NEXT_PUBLIC_BASE_URL : "") +
-    `/avatar/${filename}${query}`
+    `/avatar/${filename}`
   );
 }
